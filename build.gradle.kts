b040f2ed4a1a238ae19ef8c9fbbf758b8c6622a8
MeershoekHans <hnsmrshk@gmail.com>
2025-03-30 18:53:37 +0200

    Add root build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.48")
+        classpath("com.google.gms:google-services:4.4.0")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
+    }
+}
+
+plugins {
+    id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
